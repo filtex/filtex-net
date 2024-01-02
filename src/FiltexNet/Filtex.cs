6c50eca8a73b0bdf40cb2a3df33efb7ddcd93061
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-02 23:42:22 +0300

    feat: create filtex type and ctor with options

---
+++ src/FiltexNet/Filtex.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using FiltexNet.Models;
+using FiltexNet.Options;
+
+namespace FiltexNet
+{
+    public class Filtex
+    {
+        private readonly Metadata _metadata;
+
+        private Filtex(Metadata metadata)
+        {
+            _metadata = metadata;
+        }
+
+        public static Filtex New(params IOption[] options)
+        {
+            var lookups = options
+                .OfType<LookupOption>()
+                .Select(x => x.Build())
+                .ToArray();
+
+            var metadata = new Metadata
+            {
+                Fields = options
+                    .OfType<FieldOption>()
+                    .Select(x => x.Build(lookups))
+                    .ToArray()
+            };
+
+            return new Filtex(metadata);
+        }
+
+        public Metadata Metadata()
+        {
+            return _metadata;
+        }
+    }
+}
