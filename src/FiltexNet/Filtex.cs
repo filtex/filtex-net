b6a2888ba79dd700092a80de2da95ea2425aeac7
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-02 23:53:26 +0300

    feat: add parsing and validating funcs to filtex type

--- src/FiltexNet/Filtex.cs
+++ src/FiltexNet/Filtex.cs
@@ -1,6 +1,10 @@
 ﻿using System.Linq;
+using FiltexNet.Expressions;
 using FiltexNet.Models;
 using FiltexNet.Options;
+using FiltexNet.Parsers;
+using FiltexNet.Tokenizers;
+using FiltexNet.Validators;
 
 namespace FiltexNet
 {
@@ -34,6 +38,26 @@ namespace FiltexNet
         public Metadata Metadata()
         {
             return _metadata;
+        }
+
+        public IExpression ExpressionFromJson(string jsonQuery)
+        {
+            return new JsonQueryParser(_metadata, new JsonQueryTokenizer(_metadata)).Parse(jsonQuery);
+        }
+
+        public IExpression ExpressionFromText(string textQuery)
+        {
+            return new TextQueryParser(_metadata, new TextQueryTokenizer(_metadata)).Parse(textQuery);
+        }
+
+        public void ValidateFromJson(string jsonQuery)
+        {
+            new JsonQueryValidator(_metadata, new JsonQueryTokenizer(_metadata)).Validate(jsonQuery);
+        }
+
+        public void ValidateFromText(string textQuery)
+        {
+            new TextQueryValidator(_metadata, new TextQueryTokenizer(_metadata)).Validate(textQuery);
         }
     }
 }
