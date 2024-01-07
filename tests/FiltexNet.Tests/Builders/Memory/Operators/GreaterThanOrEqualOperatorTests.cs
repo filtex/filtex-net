68f493ba7376e28ba85f60a681716b63ae314282
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-07 16:07:19 +0300

    test: add memory greater and greater equal operator tests

---
+++ tests/FiltexNet.Tests/Builders/Memory/Operators/GreaterThanOrEqualOperatorTests.cs
@@ -0,0 +1,439 @@
+using System;
+using FiltexNet.Builders.Memory.Operators;
+using FiltexNet.Builders.Memory.Utils;
+using FiltexNet.Constants;
+using Xunit;
+
+namespace FiltexNet.Tests.Builders.Memory.Operators
+{
+    public class GreaterThanOrEqualOperatorTests
+    {
+        [Fact]
+        public void Build_ShouldReturnFalse_WhenFieldTypeIsNumberAndNull()
+        {
+            // Arrange
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = (decimal?)null
+            });
+            var expression = GreaterThanOrEqualOperator.Build(FieldType.FieldTypeNumber, "Value", (decimal)100);
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnFalse_WhenFieldTypeIsNumberAndLess()
+        {
+            // Arrange
+            var value = (decimal)99;
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = value
+            });
+            var expression = GreaterThanOrEqualOperator.Build(FieldType.FieldTypeNumber, "Value", value + 1);
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnTrue_WhenFieldTypeIsNumberAndEqual()
+        {
+            // Arrange
+            var value = (decimal)100;
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = value
+            });
+            var expression = GreaterThanOrEqualOperator.Build(FieldType.FieldTypeNumber, "Value", value);
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnTrue_WhenFieldTypeIsNumberAndGreater()
+        {
+            // Arrange
+            var value = (decimal)100;
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = value
+            });
+            var expression = GreaterThanOrEqualOperator.Build(FieldType.FieldTypeNumber, "Value", value - 1);
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnFalse_WhenFieldTypeIsDateAndNull()
+        {
+            // Arrange
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = (DateTime?)null
+            });
+            var expression = GreaterThanOrEqualOperator.Build(FieldType.FieldTypeDate, "Value", DateTime.Now);
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnFalse_WhenFieldTypeIsDateAndLess()
+        {
+            // Arrange
+            var now = DateTime.UtcNow;
+            var value = new DateTime(now.Year, now.Month, now.Day);
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = value
+            });
+            var expression = GreaterThanOrEqualOperator.Build(FieldType.FieldTypeDate, "Value", value.AddDays(1));
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnTrue_WhenFieldTypeIsDateAndEqual()
+        {
+            // Arrange
+            var now = DateTime.UtcNow;
+            var value = new DateTime(now.Year, now.Month, now.Day);
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = value
+            });
+            var expression = GreaterThanOrEqualOperator.Build(FieldType.FieldTypeDate, "Value", value);
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnTrue_WhenFieldTypeIsDateAndGreater()
+        {
+            // Arrange
+            var now = DateTime.UtcNow;
+            var value = new DateTime(now.Year, now.Month, now.Day);
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = value
+            });
+            var expression = GreaterThanOrEqualOperator.Build(FieldType.FieldTypeDate, "Value", value.AddDays(-1));
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnFalse_WhenFieldTypeIsTimeAndNull()
+        {
+            // Arrange
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = (int?)null
+            });
+            var expression = GreaterThanOrEqualOperator.Build(FieldType.FieldTypeTime, "Value", 60);
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnFalse_WhenFieldTypeIsTimeAndLess()
+        {
+            // Arrange
+            var value = 60;
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = value
+            });
+            var expression = GreaterThanOrEqualOperator.Build(FieldType.FieldTypeTime, "Value", value + 1);
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnTrue_WhenFieldTypeIsTimeAndEqual()
+        {
+            // Arrange
+            var value = 60;
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = value
+            });
+            var expression = GreaterThanOrEqualOperator.Build(FieldType.FieldTypeTime, "Value", value);
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnTrue_WhenFieldTypeIsTimeAndGreater()
+        {
+            // Arrange
+            var value = 60;
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = value
+            });
+            var expression =
+                GreaterThanOrEqualOperator.Build(FieldType.FieldTypeTime, "Value", value - 1);
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnFalse_WhenFieldTypeIsDateTimeAndNull()
+        {
+            // Arrange
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = (DateTime?)null
+            });
+            var expression = GreaterThanOrEqualOperator.Build(FieldType.FieldTypeDateTime, "Value", DateTime.Now);
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnFalse_WhenFieldTypeIsDateTimeAndLess()
+        {
+            // Arrange
+            var now = DateTime.UtcNow;
+            var value = new DateTime(now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second);
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = value
+            });
+            var expression = GreaterThanOrEqualOperator.Build(FieldType.FieldTypeDateTime, "Value", value.AddHours(24));
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnTrue_WhenFieldTypeIsDateTimeAndEqual()
+        {
+            // Arrange
+            var now = DateTime.UtcNow;
+            var value = new DateTime(now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second);
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = value
+            });
+            var expression = GreaterThanOrEqualOperator.Build(FieldType.FieldTypeDateTime, "Value", value);
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnTrue_WhenFieldTypeIsDateTimeAndGreater()
+        {
+            // Arrange
+            var now = DateTime.UtcNow;
+            var value = new DateTime(now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second);
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = value
+            });
+            var expression =
+                GreaterThanOrEqualOperator.Build(FieldType.FieldTypeDateTime, "Value", value.AddHours(-24));
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnFalse_WhenFieldTypeIsString()
+        {
+            // Arrange
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = "Filtex"
+            });
+            var expression = GreaterThanOrEqualOperator.Build(FieldType.FieldTypeString, "Value", null);
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnFalse_WhenFieldTypeIsStringArray()
+        {
+            // Arrange
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = (string[])null
+            });
+            var expression = GreaterThanOrEqualOperator.Build(FieldType.FieldTypeStringArray, "Value", null);
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnFalse_WhenFieldTypeIsBoolean()
+        {
+            // Arrange
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = false
+            });
+            var expression = GreaterThanOrEqualOperator.Build(FieldType.FieldTypeBoolean, "Value", null);
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnFalse_WhenFieldTypeIsBooleanArray()
+        {
+            // Arrange
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = (bool[])null
+            });
+            var expression = GreaterThanOrEqualOperator.Build(FieldType.FieldTypeBooleanArray, "Value", null);
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnFalse_WhenFieldTypeIsNumberArray()
+        {
+            // Arrange
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = (int[])null
+            });
+            var expression = GreaterThanOrEqualOperator.Build(FieldType.FieldTypeNumberArray, "Value", null);
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnFalse_WhenFieldTypeIsDateArray()
+        {
+            // Arrange
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = (DateTime[])null
+            });
+            var expression = GreaterThanOrEqualOperator.Build(FieldType.FieldTypeDateArray, "Value", null);
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnFalse_WhenFieldTypeIsTimeArray()
+        {
+            // Arrange
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = (int[])null
+            });
+            var expression = GreaterThanOrEqualOperator.Build(FieldType.FieldTypeTimeArray, "Value", null);
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnFalse_WhenFieldTypeIsDateTimeArray()
+        {
+            // Arrange
+            var data = MemoryUtils.ObjectToDictionary(new
+            {
+                Value = (DateTime[])null
+            });
+            var expression = GreaterThanOrEqualOperator.Build(FieldType.FieldTypeDateTimeArray, "Value", null);
+
+            // Act
+            var result = expression.Fn(data);
+
+            // Assert
+            Assert.False(result);
+        }
+    }
+}
