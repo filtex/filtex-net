c1e7b3d4556d1362a17c1981afb4469e2b347777
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-07 16:28:08 +0300

    test: add postgres end with and not end with operator tests

---
+++ tests/FiltexNet.Tests/Builders/Postgres/Operators/NotEndWithOperatorTests.cs
@@ -0,0 +1,146 @@
+using FiltexNet.Builders.Postgres.Operators;
+using FiltexNet.Constants;
+using Xunit;
+
+namespace FiltexNet.Tests.Builders.Postgres.Operators
+{
+    public class NotEndWithOperatorTests
+    {
+        [Fact]
+        public void Build_ShouldReturnExpression_WhenFieldTypeIsString()
+        {
+            // Arrange
+            var value = "Filtex";
+
+            // Act
+            var expression = NotEndWithOperator.Build(FieldType.FieldTypeString, "Value", value, 1);
+
+            // Assert
+            Assert.NotNull(expression);
+            Assert.Equal("Value NOT ILIKE '%' || $1", expression.Condition);
+            Assert.Single(expression.Args);
+            Assert.Equal(value, expression.Args[0]);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnNull_WhenFieldTypeIsStringArray()
+        {
+            // Arrange
+            // Act
+            var expression = NotEndWithOperator.Build(FieldType.FieldTypeStringArray, "Value", null, 0);
+
+            // Assert
+            Assert.Null(expression);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnNull_WhenFieldTypeIsNumber()
+        {
+            // Arrange
+            // Act
+            var expression = NotEndWithOperator.Build(FieldType.FieldTypeNumber, "Value", null, 0);
+
+            // Assert
+            Assert.Null(expression);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnNull_WhenFieldTypeIsNumberArray()
+        {
+            // Arrange
+            // Act
+            var expression = NotEndWithOperator.Build(FieldType.FieldTypeNumberArray, "Value", null, 0);
+
+            // Assert
+            Assert.Null(expression);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnNull_WhenFieldTypeIsBoolean()
+        {
+            // Arrange
+            // Act
+            var expression = NotEndWithOperator.Build(FieldType.FieldTypeBoolean, "Value", null, 0);
+
+            // Assert
+            Assert.Null(expression);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnNull_WhenFieldTypeIsBooleanArray()
+        {
+            // Arrange
+            // Act
+            var expression = NotEndWithOperator.Build(FieldType.FieldTypeBooleanArray, "Value", null, 0);
+
+            // Assert
+            Assert.Null(expression);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnNull_WhenFieldTypeIsDate()
+        {
+            // Arrange
+            // Act
+            var expression = NotEndWithOperator.Build(FieldType.FieldTypeDate, "Value", null, 0);
+
+            // Assert
+            Assert.Null(expression);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnNull_WhenFieldTypeIsDateArray()
+        {
+            // Arrange
+            // Act
+            var expression = NotEndWithOperator.Build(FieldType.FieldTypeDateArray, "Value", null, 0);
+
+            // Assert
+            Assert.Null(expression);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnNull_WhenFieldTypeIsTime()
+        {
+            // Arrange
+            // Act
+            var expression = NotEndWithOperator.Build(FieldType.FieldTypeTime, "Value", null, 0);
+
+            // Assert
+            Assert.Null(expression);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnNull_WhenFieldTypeIsTimeArray()
+        {
+            // Arrange
+            // Act
+            var expression = NotEndWithOperator.Build(FieldType.FieldTypeTimeArray, "Value", null, 0);
+
+            // Assert
+            Assert.Null(expression);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnNull_WhenFieldTypeIsDateTime()
+        {
+            // Arrange
+            // Act
+            var expression = NotEndWithOperator.Build(FieldType.FieldTypeDateTime, "Value", null, 0);
+
+            // Assert
+            Assert.Null(expression);
+        }
+
+        [Fact]
+        public void Build_ShouldReturnNull_WhenFieldTypeIsDateTimeArray()
+        {
+            // Arrange
+            // Act
+            var expression = NotEndWithOperator.Build(FieldType.FieldTypeDateTimeArray, "Value", null, 0);
+
+            // Assert
+            Assert.Null(expression);
+        }
+    }
+}
