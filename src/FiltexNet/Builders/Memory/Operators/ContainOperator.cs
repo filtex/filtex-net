286091af38e41772f8ff292330b7bfd464e93b46
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-07 16:08:10 +0300

    feat: create memory contain and not contain operator impls

---
+++ src/FiltexNet/Builders/Memory/Operators/ContainOperator.cs
@@ -0,0 +1,55 @@
+using FiltexNet.Builders.Memory.Types;
+using FiltexNet.Builders.Memory.Utils;
+using FiltexNet.Constants;
+using FiltexNet.Utils;
+
+namespace FiltexNet.Builders.Memory.Operators
+{
+    public static class ContainOperator
+    {
+        public static MemoryExpression Build(FieldType fieldType, string field, object value)
+        {
+            if (fieldType.IsArray())
+            {
+                return new MemoryExpression
+                {
+                    Fn = data =>
+                    {
+                        if (!data.TryGetValue(field, out var val))
+                        {
+                            return false;
+                        }
+
+                        foreach (var v in CastUtils.Array(val))
+                        {
+                            if (MemoryUtils.CheckEquality(fieldType, v, value))
+                            {
+                                return true;
+                            }
+                        }
+
+                        return false;
+                    }
+                };
+            }
+
+            return new MemoryExpression
+            {
+                Fn = data =>
+                {
+                    if (!data.TryGetValue(field, out var val))
+                    {
+                        return false;
+                    }
+
+                    if (fieldType.Name == FieldType.FieldTypeString.Name)
+                    {
+                        return val?.ToString()?.ToLowerInvariant().Contains(value?.ToString()?.ToLowerInvariant() ?? string.Empty) == true;
+                    }
+
+                    return false;
+                }
+            };
+        }
+    }
+}
