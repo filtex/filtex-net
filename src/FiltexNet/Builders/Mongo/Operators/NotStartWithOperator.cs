58c99fd82305ab429a7c1b6c2e4e0043951b66d8
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-07 16:12:48 +0300

    feat: create mongo start with and not start with operator impls

---
+++ src/FiltexNet/Builders/Mongo/Operators/NotStartWithOperator.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using FiltexNet.Builders.Mongo.Types;
+using FiltexNet.Constants;
+using MongoDB.Bson;
+
+namespace FiltexNet.Builders.Mongo.Operators
+{
+    public static class NotStartWithOperator
+    {
+        public static MongoExpression Build(FieldType fieldType, string field, object value)
+        {
+            if (fieldType.Name != FieldType.FieldTypeString.Name)
+            {
+                return null;
+            }
+
+            return new MongoExpression
+            {
+                Condition = new BsonDocument(field, new BsonDocument("$not", new BsonDocument(new List<BsonElement>
+                {
+                    new("$regex", BsonValue.Create($"^{value}")),
+                    new("$options", BsonValue.Create("i"))
+                })))
+            };
+        }
+    }
+}
