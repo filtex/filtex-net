7eace8e8279ba29c280d39106ab2b62ead628d42
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-07 16:26:47 +0300

    test: add postgres and logic tests

---
+++ tests/FiltexNet.Tests/Builders/Postgres/Logics/AndLogicTests.cs
@@ -0,0 +1,29 @@
+using FiltexNet.Builders.Postgres.Logics;
+using FiltexNet.Builders.Postgres.Operators;
+using FiltexNet.Constants;
+using Xunit;
+
+namespace FiltexNet.Tests.Builders.Postgres.Logics
+{
+    public class AndLogicTests
+    {
+        [Fact]
+        public void Build_ShouldReturnExpression_WhenThereAreExpressions()
+        {
+            // Arrange
+            var value = "Filtex";
+
+            // Act
+            var expression = AndLogic.Build(new[]
+            {
+                EqualOperator.Build(FieldType.FieldTypeString, "Value", value, 0)
+            });
+
+            // Assert
+            Assert.NotNull(expression);
+            Assert.NotEmpty(expression.Condition);
+            Assert.Single(expression.Args);
+            Assert.Equal(value, expression.Args[0]);
+        }
+    }
+}
