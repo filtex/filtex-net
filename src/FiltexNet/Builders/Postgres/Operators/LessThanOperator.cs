97c1ccaba78318b6af72ae3836132bf00eb6c3f5
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-07 16:28:18 +0300

    feat: create postgres less and less equal operator impls

---
+++ src/FiltexNet/Builders/Postgres/Operators/LessThanOperator.cs
@@ -0,0 +1,25 @@
+using FiltexNet.Builders.Postgres.Types;
+using FiltexNet.Constants;
+
+namespace FiltexNet.Builders.Postgres.Operators
+{
+    public static class LessThanOperator
+    {
+        public static PostgresExpression Build(FieldType fieldType, string field, object value, int index)
+        {
+            if (fieldType.Name != FieldType.FieldTypeNumber.Name &&
+                fieldType.Name != FieldType.FieldTypeDate.Name &&
+                fieldType.Name != FieldType.FieldTypeTime.Name &&
+                fieldType.Name != FieldType.FieldTypeDateTime.Name)
+            {
+                return null;
+            }
+
+            return new PostgresExpression
+            {
+                Condition = $"{field} < ${index}",
+                Args = new[] { value }
+            };
+        }
+    }
+}
