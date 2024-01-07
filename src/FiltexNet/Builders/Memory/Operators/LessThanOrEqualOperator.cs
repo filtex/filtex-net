1ad495d52e14a36b318495d3ea894f9b8c34d4e9
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-07 16:06:49 +0300

    feat: create memory less and less equal operator impls

---
+++ src/FiltexNet/Builders/Memory/Operators/LessThanOrEqualOperator.cs
@@ -0,0 +1,50 @@
+using FiltexNet.Builders.Memory.Types;
+using FiltexNet.Constants;
+using FiltexNet.Utils;
+
+namespace FiltexNet.Builders.Memory.Operators
+{
+    public static class LessThanOrEqualOperator
+    {
+        public static MemoryExpression Build(FieldType fieldType, string field, object value)
+        {
+            return new MemoryExpression
+            {
+                Fn = data =>
+                {
+                    if (!data.TryGetValue(field, out var val))
+                    {
+                        return false;
+                    }
+
+                    if (val == null)
+                    {
+                        return false;
+                    }
+
+                    if (fieldType.Name == FieldType.FieldTypeNumber.Name)
+                    {
+                        return CastUtils.Number(val) <= CastUtils.Number(value);
+                    }
+
+                    if (fieldType.Name == FieldType.FieldTypeDate.Name)
+                    {
+                        return CastUtils.Date(val) <= CastUtils.Date(value);
+                    }
+
+                    if (fieldType.Name == FieldType.FieldTypeTime.Name)
+                    {
+                        return CastUtils.Time(val) <= CastUtils.Time(value);
+                    }
+
+                    if (fieldType.Name == FieldType.FieldTypeDateTime.Name)
+                    {
+                        return CastUtils.DateTime(val) <= CastUtils.DateTime(value);
+                    }
+
+                    return false;
+                }
+            };
+        }
+    }
+}
