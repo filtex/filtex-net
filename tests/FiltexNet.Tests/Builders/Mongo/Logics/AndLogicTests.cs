6aac4141d11d8d00395c19c8a79a009f76341b60
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-07 16:11:58 +0300

    test: add mongo and logic tests

---
+++ tests/FiltexNet.Tests/Builders/Mongo/Logics/AndLogicTests.cs
@@ -0,0 +1,30 @@
+using FiltexNet.Builders.Mongo.Logics;
+using FiltexNet.Builders.Mongo.Operators;
+using FiltexNet.Constants;
+using Xunit;
+
+namespace FiltexNet.Tests.Builders.Mongo.Logics
+{
+    public class AndLogicTests
+    {
+        [Fact]
+        public void Build_ShouldReturnExpression_WhenThereAreExpressions()
+        {
+            // Arrange
+            // Act
+            var expression = AndLogic.Build(new[]
+            {
+                EqualOperator.Build(FieldType.FieldTypeString, "Value", "Filtex")
+            });
+
+            // Assert
+            Assert.NotNull(expression);
+            Assert.NotNull(expression.Condition);
+
+            var value = expression.Condition["$and"];
+            Assert.NotNull(value);
+            var array = value.AsBsonArray;
+            Assert.Single(array);
+        }
+    }
+}
