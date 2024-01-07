fa7d6d8447174161793ed7bf1af50468c85a8bcc
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-07 16:06:29 +0300

    feat: create memory end with and not end with operator impls

---
+++ src/FiltexNet/Builders/Memory/Operators/NotEndWithOperator.cs
@@ -0,0 +1,29 @@
+using FiltexNet.Builders.Memory.Types;
+using FiltexNet.Constants;
+
+namespace FiltexNet.Builders.Memory.Operators
+{
+    public static class NotEndWithOperator
+    {
+        public static MemoryExpression Build(FieldType fieldType, string field, object value)
+        {
+            return new MemoryExpression
+            {
+                Fn = data =>
+                {
+                    if (!data.TryGetValue(field, out var val))
+                    {
+                        return false;
+                    }
+
+                    if (fieldType.Name == FieldType.FieldTypeString.Name)
+                    {
+                        return val?.ToString()?.ToLowerInvariant().EndsWith(value?.ToString()?.ToLowerInvariant() ?? string.Empty) != true;
+                    }
+
+                    return false;
+                }
+            };
+        }
+    }
+}
